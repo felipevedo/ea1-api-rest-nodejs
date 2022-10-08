c0a4b87e01160f707e0116fad3163828f5d51212
Felipe Acevedo <acevedo.felipe201@gmail.com>
2022-10-08 13:47:25 -0500

    add logging to dbUtil 2

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,12 +1,13 @@
 exports.init = () => {
     const db = require('mysql2');
-
+    
     const conn = db.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PW,
         database: process.env.DB_NAME
     });
+    console.log('conn', conn)
 
     function query(sql) {
         return new Promise((resolve, reject) => {
